78268747cff93836e909f9fa543306163e8f5983
Matthew-Han <837204579@qq.com>
2020-04-16 16:52:32 +0800

    Can you feel it?

--- src/main/java/xyz/yuanmo/MergeSortedArray.java
+++ src/main/java/xyz/yuanmo/MergeSortedArray.java
@@ -12,24 +12,48 @@ import java.util.Arrays;
 public class MergeSortedArray {
 
     /**
-     * 实际是归并排序的后半段
+     * 执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗 : 39.9 MB , 在所有 Java 提交中击败了 5.06% 的用户
+     * 没学过归并排序，听说这题实际算是小归并排序
+     * 看了下图解，尝试写了下，执行用时很满意~
      * @param nums1
      * @param m
      * @param nums2
      * @param n
      */
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        int i = 0;
+        int j = 0;
+        int k = 0;
         int[] temp = new int[m + n];
-        for (int i = 0; i < n; i++) {
-            nums1[m] = nums2[i];
-            m++;
+        while (i < m + n) {
+           if (j < m && k != n && nums1[j] <= nums2[k]) {
+               temp[i] = nums1[j];
+               j++;
+           } else if (k < n && j != m && nums2[k] < nums1[j]) {
+               temp[i] = nums2[k];
+               k++;
+           } else if (k < n && j == m){
+               temp[i] = nums2[k];
+               k++;
+
+           } else if (j < m && k == n){
+               temp[i] = nums1[j];
+               j++;
+           }
+           i++;
         }
+        // 第一种赋值方法
+        for (int i1 = 0; i1 < nums1.length; i1++) {
+            nums1[i1] = temp[i1];
+        }
+        // 第二种赋值方法
+        System.arraycopy(temp, 0, nums1, 0, nums1.length);
+
         System.out.println(Arrays.toString(nums1));
     }
 
     public static void main(String[] args) {
-        merge(new int[]{1,2,4,5,0,0},  4, new int[]{1,7}, 2);
-
+        merge(new int[]{1,2,3,0,0,0},  3, new int[]{2,5,6}, 3);
     }
 }
---
+++ src/main/java/xyz/yuanmo/PascalTriangle.java
@@ -0,0 +1,60 @@
+package xyz.yuanmo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName PascalTriangle
+ * @Description #118 杨辉三角
+ * @Author MatthewHan
+ * @Date 2020/4/16 15:50
+ * @Version 1.0
+ **/
+public class PascalTriangle {
+
+    /**
+     * 执行用时 : 19 ms , 在所有 Java 提交中击败了 84.61% 的用户
+     * 内存消耗 : 39.6 MB , 在所有 Java 提交中击败了 9.09% 的用户
+     * @param numRows
+     * @return
+     */
+    public static List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> lover = new ArrayList<>(numRows);
+        if (numRows == 0) {
+            return lover;
+        }
+        List<Integer> fucker = new ArrayList<>(1);
+        fucker.add(1);
+        lover.add(fucker);
+        int nextValue;
+
+        if (numRows == 1) {
+            return lover;
+        }
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> nextFucker = new ArrayList<>(numRows);
+            for (int j = 0; j < fucker.size(); j++) {
+                if (j == 0) {
+                    nextFucker.add(1);
+                }
+                if (j == fucker.size() - 1){
+                    nextFucker.add(1);
+                }
+                if (j + 1 < fucker.size()){
+                    nextValue = fucker.get(j) + fucker.get(j + 1);
+                    nextFucker.add(nextValue);
+                }
+                System.out.println("nf = " + nextFucker);
+            }
+            fucker = nextFucker;
+            lover.add(fucker);
+        }
+        return lover;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generate(0));
+
+    }
+}
