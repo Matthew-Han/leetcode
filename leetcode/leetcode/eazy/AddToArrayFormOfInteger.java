6494ea96990be64f1427f77231baebb97b906923
Matthew-Han <837204579@qq.com>
2020-04-23 15:08:40 +0800

    modify structure

--- leetcode/leetcode/eazy/AddToArrayFormOfInteger.java
+++ leetcode/leetcode/eazy/AddToArrayFormOfInteger.java
@@ -15,36 +15,40 @@ public class AddToArrayFormOfInteger {
 
 
     /**
-     * 该方法超时时间限制。。。
-     * @param A
-     * @param K
+     * 该方法，LeetCode判定超时
+     * @param a
+     * @param k
      * @return
      */
-    public static List<Integer> addToArrayForm(int[] A, int K) {
+    public static List<Integer> addToArrayForm(int[] a, int k) {
 
         StringBuilder stringBuilder = new StringBuilder();
-        for (int value : A) {
+        for (int value : a) {
             stringBuilder.append(value);
         }
         stringBuilder.reverse();
-        Integer[] left = new Integer[A.length];
+        Integer[] left = new Integer[a.length];
         for (int i = 0; i < stringBuilder.toString().split("").length; i++) {
             left[i] = Integer.parseInt(stringBuilder.toString().split("")[i]);
         }
+        System.out.println(Arrays.toString(left));
 
-        String[] k1 = String.valueOf(K).split("");
+        String[] k1 = String.valueOf(k).split("");
         Integer[] right = new Integer[k1.length];
         int l = 0;
         for (int i = right.length - 1; i >= 0; i--) {
             right[i] = Integer.parseInt(k1[l]);
             l++;
         }
+        System.out.println(Arrays.toString(right));
 
+        /* ======================================================= */
         int size = Math.max(left.length, right.length) + 1;
         Integer[] result = new Integer[size];
         for (int i = 0; i < result.length; i++) {
             result[i] = (i < left.length ? left[i] : 0) + (i < right.length ? right[i] : 0);
         }
+        System.out.println(Arrays.toString(result));
 
 
         for (int i = 0; i < result.length - 1; i++) {
@@ -53,7 +57,9 @@ public class AddToArrayFormOfInteger {
                 result[i + 1]++;
             }
         }
+        System.out.println(Arrays.toString(result));
 
+        /* ======================================================= */
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < result.length; i++) {
             if (i != result.length - 1 || (i == result.length - 1 && result[result.length - 1] != 0)) {
