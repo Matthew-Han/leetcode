daff2d4714a13fef39b0f4a339d9a8796cdf8acb
Matthew-Han <837204579@qq.com>
2020-04-29 14:58:26 +0800

    #141 环形链表

--- eazy/LinkedListCycle.java
+++ eazy/LinkedListCycle.java
@@ -7,14 +7,53 @@
  **/
 public class LinkedListCycle {
 
+    /**
+     * 快慢双指针
+     * 执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗 : 40.1 MB , 在所有 Java 提交中击败了 5.49% 的用户
+     *
+     * @param head
+     * @return
+     */
+    public static boolean hasCycle(ListNode141 head) {
+
+        ListNode141 quick = head;
+        ListNode141 slow = head;
+
+        while (slow != null && quick.next != null) {
+            if (null != quick.next.next) {
+                quick = quick.next.next;
+                slow = slow.next;
+            } else {
+                return false;
+            }
+            if (slow.val == quick.val) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode141 listNode = new ListNode141(1);
+        listNode.next = new ListNode141(2);
+        listNode.next.next = new ListNode141(3);
+        listNode.next.next.next = new ListNode141(4);
+        listNode.next.next.next.next = listNode.next.next.next;
+
+        System.out.println(hasCycle(listNode));
+    }
 
 
 }
+
 class ListNode141 {
     int val;
     ListNode141 next;
+
     ListNode141(int x) {
         val = x;
         next = null;
     }
+
 }
