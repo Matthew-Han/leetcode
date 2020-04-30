726b632e46dfc9f05a36d70060688077e320fbb6
Matthew-Han <837204579@qq.com>
2020-04-30 10:34:24 +0800

    链表公共类

---
+++ eazy/链表/ListNode.java
@@ -0,0 +1,14 @@
+package 链表;
+
+/**
+ * @ClassName 链表.ListNode
+ * @Description 链表公共类
+ * @Author MatthewHan
+ * @Date 2020/4/30 10:28
+ * @Version 1.0
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
