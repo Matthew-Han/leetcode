bbb5d8e588552b6edc552efcadca65390eeb5aef
Matthew-Han <837204579@qq.com>
2020-05-11 11:31:42 +0800

    #367 有效的完全平方数

--- eazy/二分法/ValidPerfectSquare.java
+++ eazy/二分法/ValidPerfectSquare.java
@@ -1,5 +1,7 @@
+package 二分法;
+
 /**
- * @ClassName ValidPerfectSquare
+ * @ClassName 二分法.ValidPerfectSquare
  * @Description #367 有效的完全平方数
  * @Author MatthewHan
  * @Date 2020/5/9 18:01
@@ -36,22 +38,40 @@ public class ValidPerfectSquare {
     /**
      * int整数最大是2的31次方-1，大约21亿，只要根据整数的长度可以确定他的平方根大概落在某个区间
      * 比如2位数（10~99），平方根一定是在1~10，比如三位数（100~999），平方根一定是在（10~32），比如四位数（1000~9999），平方根一定是落在（31~100）
-     * 当这个数很大的话（最大为2147483647），相应的区间也会变大，最坏的情况要计算（46341 - 31622）次
+     * 当这个数很大的话（最大为2147483647），相应的区间也会变大
+     * 加入了二分法后，可以快速避免。
      * <p>
-     * 执行用时 : 1 ms , 在所有 Java 提交中击败了 28.75% 的用户
-     * 内存消耗 : 36 MB , 在所有 Java 提交中击败了 9.52% 的用户
+     * 执行用时 : 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗 : 36.6 MB , 在所有 Java 提交中击败了 9.52% 的用户
      *
      * @param num
      * @return
      */
     public static boolean isPerfectSquarePro(int num) {
         int[] se = interval(String.valueOf(num).length());
-        for (int i = se[0]; i <= se[1]; i++) {
-            if (i * i == num) {
+        int left = se[0];
+        int right = se[1];
+
+        while (true) {
+            int mid = left + (right - left) / 2;
+            System.out.println("left:" + left);
+            System.out.println("right:" + right);
+            if (mid * mid < num) {
+                if (right - left == 1) {
+                    return false;
+                } else {
+                    left = mid;
+                }
+            } else if (mid * mid == num) {
                 return true;
+            } else {
+                if (right - left == 1) {
+                    return false;
+                } else {
+                    right = mid;
+                }
             }
         }
-        return false;
     }
 
     public static int[] interval(int len) {
@@ -98,7 +118,8 @@ public class ValidPerfectSquare {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPerfectSquarePro(2147483647));
+        System.out.println(isPerfectSquarePro(99));
         System.out.println(Math.sqrt(2147483647));
+
     }
 }
