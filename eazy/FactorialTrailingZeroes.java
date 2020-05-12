3af4d5e68b6f76039609a4b4284d79d4022fb5dc
Matthew-Han <837204579@qq.com>
2020-05-12 10:29:49 +0800

    #172 阶乘后的零

--- eazy/FactorialTrailingZeroes.java
+++ eazy/FactorialTrailingZeroes.java
@@ -10,13 +10,14 @@ public class FactorialTrailingZeroes {
     /**
      * 算多少个5
      * 但是当有个25时，拆分成2个5相乘，所以30的时候是7个5
+     *
      * @param n
      * @return
      */
     public static int trailingZeroes(int n) {
 
         int count = 0;
-        while(n >= 5) {
+        while (n >= 5) {
             count += n / 5;
             n /= 5;
         }
