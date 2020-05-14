2af9342242280950d7102082a646a49a6942ef5d
Matthew-Han <837204579@qq.com>
2020-05-14 14:21:21 +0800

    #401 二进制手表

--- eazy/ConvertAnumberToHexadecimal.java
+++ eazy/ConvertAnumberToHexadecimal.java
@@ -55,7 +55,7 @@ public class ConvertAnumberToHexadecimal {
      */
     public static String toHexPro(int num) {
 
-        char [] array = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        char[] array = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
         if (num == 0) {
             return "0";
         }
