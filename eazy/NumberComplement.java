365aa00c44761c70df553d4a57bddccf75bcf058
Matthew-Han <837204579@qq.com>
2020-05-17 21:01:10 +0800

    #476 数字的补数

---
+++ eazy/NumberComplement.java
@@ -0,0 +1,30 @@
+/**
+ * @ClassName NumberComplement
+ * @Description #476 数字的补数
+ * @Author MatthewHan
+ * @Date 2020/5/17 20:46
+ * @Version 1.0
+ **/
+public class NumberComplement {
+
+    /**
+     * 执行用时 : 7 ms , 在所有 Java 提交中击败了 5.72% 的用户
+     * 内存消耗 : 37.3 MB , 在所有 Java 提交中击败了 7.69% 的用户
+     *
+     * @param num
+     * @return
+     */
+    public static int findComplement(int num) {
+        String[] strings = Integer.toBinaryString(num).split("");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String string : strings) {
+            stringBuilder.append(Integer.parseInt(string) == 1 ? 0 : (Integer.parseInt(string) == 0 ? 1 : 0));
+        }
+        System.out.println(stringBuilder);
+        return Integer.parseInt(stringBuilder.toString(), 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findComplement(5));
+    }
+}
