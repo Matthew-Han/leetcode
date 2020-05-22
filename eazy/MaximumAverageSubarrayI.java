b6134a32587dd54c86f949b142b0637d85c29664
Matthew-Han <837204579@qq.com>
2020-05-22 22:29:32 +0800

    #643 子数组最大平均数 I

--- eazy/MaximumAverageSubarrayI.java
+++ eazy/MaximumAverageSubarrayI.java
@@ -8,7 +8,11 @@
 public class MaximumAverageSubarrayI {
 
     /**
-     * 执行用时 : 6 ms , 在所有 Java 提交中击败了 36.90% 的用户
+     * 两个优化点：
+     * 1. 不要把k个数字的和算好后，下一轮循环又置0。利用滑动窗口双指针，下一次循环是这次的和减去这组的头元素加上next指针的元素（需要防御下标溢出）；
+     * 2. 平均数最后结果求就可以，不要每次都算出平均数然后比较较大值。（这点如果做了能优化很多）
+     * <p>
+     * 执行用时 : 4 ms , 在所有 Java 提交中击败了 75.97% 的用户
      * 内存消耗 : 43.6 MB , 在所有 Java 提交中击败了 16.67% 的用户
      *
      * @param nums
@@ -16,24 +20,23 @@ public class MaximumAverageSubarrayI {
      * @return
      */
     public static double findMaxAverage(int[] nums, int k) {
-
         int i = 0;
         int j = i + k;
         double sum = 0;
         for (int n = 0; n < k; n++) {
             sum += nums[n];
         }
-        double max = sum / k;
+        double max = sum;
         while (j < nums.length) {
             sum = (sum - nums[i] + nums[j]);
             i++;
             j++;
-            max = Math.max(max, sum / k);
+            max = Math.max(max, sum);
         }
-        return max;
+        return max / k;
     }
 
     public static void main(String[] args) {
-        System.out.println(findMaxAverage(new int[]{-1}, 1));
+        System.out.println(findMaxAverage(new int[]{-1, 3, 5, 6, 72, 123, 124, 1, 2, 3}, 4));
     }
 }
