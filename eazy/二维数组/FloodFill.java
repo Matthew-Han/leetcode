0d6b3705d1e49c2eb2c2a9c860bb8d99451668cf
Matthew-Han <837204579@qq.com>
2020-06-06 00:32:48 +0800

    #733 图像渲染

---
+++ eazy/二维数组/FloodFill.java
@@ -0,0 +1,45 @@
+package 二维数组;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName FloodFill
+ * @Description #733 图像渲染
+ * @Author MatthewHan
+ * @Date 2020/6/5 22:48
+ * @Version 1.0
+ **/
+public class FloodFill {
+
+    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) {
+            dfs(image, sr, sc, color, newColor);
+        }
+        return image;
+    }
+
+    public static void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1) {
+                dfs(image, r - 1, c, color, newColor);
+            }
+            if (c >= 1) {
+                dfs(image, r, c - 1, color, newColor);
+            }
+            if (r + 1 < image.length) {
+                dfs(image, r + 1, c, color, newColor);
+            }
+            if (c + 1 < image[0].length) {
+                dfs(image, r, c + 1, color, newColor);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(floodFill(new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}}, 1, 1, 2)));
+    }
+
+}
