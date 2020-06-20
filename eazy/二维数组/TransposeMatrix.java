6f22fd814934e54ae76f6bfa4416a0692e386a3a
Matthew-Han <837204579@qq.com>
2020-06-20 23:28:33 +0800

    #867 转置矩阵

--- eazy/二维数组/TransposeMatrix.java
+++ eazy/二维数组/TransposeMatrix.java
@@ -22,9 +22,9 @@ public class TransposeMatrix {
         int x = arr.length;
         int y = arr[0].length;
         int[][] fucker = new int[y][x];
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                fucker[j][i] = arr[i][j];
+        for (int i = 0; i < y; i++) {
+            for (int j = 0; j < x; j++) {
+                fucker[i][j] = arr[j][i];
             }
         }
         System.out.println("fucker = " + Arrays.deepToString(fucker));
