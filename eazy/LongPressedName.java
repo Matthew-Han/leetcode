12caa1972bc4e6bbe077bc01be05fe83b58930e8
Matthew-Han <837204579@qq.com>
2020-06-23 17:47:30 +0800

    #925 长按键入

---
+++ eazy/LongPressedName.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName LongPressedName
+ * @Description #925 长按键入
+ * @Author MatthewHan
+ * @Date 2020/6/23 17:08
+ * @Version 1.0
+ **/
+public class LongPressedName {
+
+    public static boolean isLongPressedName(String name, String typed) {
+
+        List<int[]> list = new ArrayList<>();
+        char[] chars = name.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
