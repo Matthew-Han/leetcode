2237dbc3b8a81b5804408a3ddfed1d2a132c97c1
Matthew-Han <837204579@qq.com>
2020-06-29 17:43:55 +0800

    #970 强整数

--- eazy/PowerfulIntegers.java
+++ eazy/PowerfulIntegers.java
@@ -24,8 +24,8 @@ public class PowerfulIntegers {
      */
     public static List<Integer> powerfulIntegers(int x, int y, int bound) {
         Set<Integer> seen = new HashSet<>();
-        for (int i = 0; i < 18 && Math.pow(x, i) <= bound; ++i) {
-            for (int j = 0; j < 18 && Math.pow(y, j) <= bound; ++j) {
+        for (int i = 0; i < 20 && Math.pow(x, i) <= bound; ++i) {
+            for (int j = 0; j < 20 && Math.pow(y, j) <= bound; ++j) {
                 int v = (int) Math.pow(x, i) + (int) Math.pow(y, j);
                 if (v <= bound) {
                     seen.add(v);
