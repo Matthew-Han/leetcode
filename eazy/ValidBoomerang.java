ba0ecc2ee6a3b2994ff6d0d7a611837ad25d720a
Matthew-Han <837204579@qq.com>
2020-07-02 18:26:11 +0800

    #1037 有效的回旋镖

--- eazy/ValidBoomerang.java
+++ eazy/ValidBoomerang.java
@@ -11,6 +11,28 @@ public class ValidBoomerang {
 
 
     /**
+     * 求斜率
+     * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗： 37.2 MB , 在所有 Java 提交中击败了 50.00% 的用户
+     *
+     * @param points
+     * @return
+     */
+    public static boolean isBoomerangPro(int[][] points) {
+        int x1 = points[0][0];
+        int y1 = points[0][1];
+
+        int x2 = points[1][0];
+        int y2 = points[1][1];
+
+        int x3 = points[2][0];
+        int y3 = points[2][1];
+
+        return (y2 - y1) * (x3 - x2) != (y3 - y2) * (x2 - x1);
+    }
+
+
+    /**
      * 执行用时： 1 ms , 在所有 Java 提交中击败了 10.03% 的用户
      * 内存消耗： 37.4 MB , 在所有 Java 提交中击败了 50.00% 的用户
      *
