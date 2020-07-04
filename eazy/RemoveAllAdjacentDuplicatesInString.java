640e3046c72406b9819f220999782f0e666c9773
Matthew-Han <837204579@qq.com>
2020-07-05 03:11:34 +0800

    #1047 删除字符串中的所有相邻重复项

--- eazy/RemoveAllAdjacentDuplicatesInString.java
+++ eazy/RemoveAllAdjacentDuplicatesInString.java
@@ -51,7 +51,7 @@ public class RemoveAllAdjacentDuplicatesInString {
     }
 
     /**
-     * 执行用时： 50 ms , 在所有 Java 提交中击败了 25.58% 的用户
+     * 执行用时： 29 ms , 在所有 Java 提交中击败了 52.96% 的用户
      * 内存消耗： 40.5 MB , 在所有 Java 提交中击败了 7.69% 的用户
      *
      * @param s
@@ -59,30 +59,23 @@ public class RemoveAllAdjacentDuplicatesInString {
      */
     public static String removeDuplicates(String s) {
         Stack<Character> stack = new Stack<>();
-        StringBuilder sb = new StringBuilder(s);
-        while (true) {
-            int len = sb.length();
-            stack.removeAllElements();
-            for (int i = 0; i < sb.length(); i++) {
-                char curr = sb.charAt(i);
-                if (stack.size() != 0 && stack.peek() == curr) {
-                    stack.pop();
-                } else {
-                    stack.push(curr);
-                }
-            }
-            sb.delete(0, sb.length());
-            for (Character character : stack) {
-                sb.append(character);
-            }
-            if (len == sb.length()) {
-                return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char curr = s.charAt(i);
+            if (stack.size() != 0 && stack.peek() == curr) {
+                stack.pop();
+            } else {
+                stack.push(curr);
             }
         }
+        for (Character character : stack) {
+            sb.append(character);
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
-        System.out.println(removeDuplicatesPro("cssaacsafvvs"));
+        System.out.println(removeDuplicates("cssaacsafvvs"));
     }
 
 }
