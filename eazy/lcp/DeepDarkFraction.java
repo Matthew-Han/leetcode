c87d7484cedb80ffa159d93ffebaee6c1d1794ac
Matthew-Han <837204579@qq.com>
2020-07-16 10:20:45 +0800

    #LCP 02 分式化简

--- eazy/lcp/DeepDarkFraction.java
+++ eazy/lcp/DeepDarkFraction.java
@@ -1,5 +1,7 @@
 package lcp;
 
+import java.util.Arrays;
+
 /**
  * @ClassName DeepDarkFraction
  * @Description #LCP 02 分式化简
@@ -10,23 +12,41 @@ package lcp;
 public class DeepDarkFraction {
 
     public static int[] fraction(int[] cont) {
-        double avg = (double) 1 / cont[cont.length - 1];
-        for (int i = cont.length - 2; i > 0; i--) {
-            avg = 1 / (cont[i] + avg);
+        int len = cont.length;
+        // cont长度为1时直接返回
+        if (len == 1) {
+            return new int[]{cont[0], 1};
         }
-        avg += cont[0];
-        System.out.println("avg = " + avg);
-        // 整数
-        if (avg % 1 == 0) {
-            return new int[]{(int) avg, 1};
-        } else {
-            int n = 2;
 
+        // 计算分式结果
+        int[] arr = new int[]{1, cont[len - 1]};
+        for (int i = len - 2; i >= 0; i--) {
+            arr = handler(cont[i], arr);
         }
-        return null;
+        int n = arr[1];
+        int m = arr[0];
+
+        // 分数化简
+        int gcd = gcd(n, m);
+        return new int[]{n / gcd, m / gcd};
     }
 
+    // 计算1/(a + arr[0]/arr[1])
+    public static int[] handler(int a, int[] arr) {
+        return new int[]{arr[1], a * arr[1] + arr[0]};
+    }
+
+    // 计算最大公约数
+    public static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        return gcd(q, p % q);
+    }
+
+
     public static void main(String[] args) {
-        fraction(new int[]{3, 2, 0, 2});
+        System.out.println(Arrays.toString(fraction(new int[]{5, 1, 0, 6, 1, 7, 2, 6, 6, 4})));
+
     }
 }
