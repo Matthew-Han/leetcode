1441cffdaa1a7834c98cd4f124874a0d03ca7e8a
Matthew-Han <837204579@qq.com>
2020-07-17 15:02:57 +0800

    链表公共类

--- eazy/链表/ListNode.java
+++ eazy/链表/ListNode.java
@@ -10,7 +10,10 @@ package 链表;
 public class ListNode {
     int val;
     ListNode next;
-    ListNode(int x) { val = x; }
+
+    public ListNode(int x) {
+        val = x;
+    }
 
     @Override
     public String toString() {
