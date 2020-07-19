f6cd0e3aedf7bbd1aa2af19ae567bdfb7dbbc733
Matthew-Han <837204579@qq.com>
2020-07-19 23:08:58 +0800

    剑指 Offer #24 反转链表 LCOF

---
+++ eazy/剑指Offer/LcOf24.java
@@ -0,0 +1,41 @@
+package 剑指Offer;
+
+import 链表.ListNode;
+
+/**
+ * @ClassName LcOf24
+ * @Description 剑指 Offer #24 反转链表 LCOF
+ * @Author MatthewHan
+ * @Date 2020/7/19 22:26
+ * @Version 1.0
+ **/
+public class LcOf24 {
+
+    /**
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList(ListNode head) {
+
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        System.out.println(reverseList(listNode));
+        System.out.println(listNode);
+
+
+    }
+}
