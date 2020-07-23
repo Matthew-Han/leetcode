35c4e8dc3841faa035a7d71ab0bcbd1f854eec26
Matthew-Han <837204579@qq.com>
2020-07-23 11:12:42 +0800

    面试题 01.01. 判定字符是否唯一

---
+++ eazy/面试题/IsUniqueLcCi.java
@@ -0,0 +1,28 @@
+package 面试题;
+
+/**
+ * @ClassName IsUniqueLcCi
+ * @Description 面试题 01.01. 判定字符是否唯一
+ * @Author MatthewHan
+ * @Date 2020/7/23 11:08
+ * @Version 1.0
+ **/
+public class IsUniqueLcCi {
+
+    /**
+     * 执行用时： 0 ms , 在所有 Java 提交中击败了 100.00% 的用户
+     * 内存消耗： 37.3 MB , 在所有 Java 提交中击败了 100.00% 的用户
+     *
+     * @param astr
+     * @return
+     */
+    public boolean isUnique(String astr) {
+        for (int i = 0; i < astr.length(); i++) {
+            if (astr.indexOf(astr.charAt(i)) != astr.lastIndexOf(astr.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
