b46c1d54d31c39930327ea6c03e4b21193e0c1a8
Matthew-Han <837204579@qq.com>
2020-07-27 17:40:53 +0800

    #509 斐波那契数

--- eazy/动态规划/FibonacciNumber.java
+++ eazy/动态规划/FibonacciNumber.java
@@ -1,6 +1,5 @@
 package 动态规划;
 
-import java.util.Arrays;
 
 /**
  * @ClassName 动态规划.FibonacciNumber
@@ -19,13 +18,12 @@ public class FibonacciNumber {
      */
     public static int fibDp(int n) {
         int[] dp = new int[n];
+        dp[0] = 0;
         dp[1] = 1;
-        dp[2] = 1;
-        for (int i = 3; i < n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
         }
-        System.out.println("dp = " + Arrays.toString(dp));
-        return dp[n - 1];
+        return dp[dp.length - 1];
     }
 
     /**
