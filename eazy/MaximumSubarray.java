0107c47329180e9b5c1a96a6368c2f090ccc19bd
Matthew-Han <837204579@qq.com>
2020-07-29 18:15:23 +0800

    #53 最大子序和

--- eazy/MaximumSubarray.java
+++ eazy/MaximumSubarray.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @ClassName MaximumSubarray
  * @Description #53 最大子序和
@@ -8,29 +10,60 @@
 public class MaximumSubarray {
 
     /**
-     * LeetCode解法
-     * 看完感觉自己是个智障
+     * 前和小于0，丢掉，取当前值存入dp，前和=dp[i]
+     * 前和大于0，无论当前值是什么，加上当前值存入dp，然后前和=dp[i]
+     * 执行用时： 1 ms , 在所有 Java 提交中击败了 95.23% 的用户
+     * 内存消耗： 39.6 MB , 在所有 Java 提交中击败了 40.45% 的用户
+     *
      * @param nums
      * @return
      */
-    public int maxSubArray(int[] nums) {
-        int ans = nums[0];
-        int sum = 0;
-        for(int num: nums) {
-            if(sum > 0) {
-                sum += num;
+    public int maxSubArray2(int[] nums) {
+        int[] dp = new int[nums.length];
+        int prev = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (prev < 0) {
+                dp[i] = nums[i];
             } else {
-                sum = num;
+                dp[i] = prev + nums[i];
+                prev += dp[i];
             }
-            ans = Math.max(ans, sum);
+            System.out.println("dp = " + Arrays.toString(dp));
         }
-        return ans;
-
-
+        int max = Integer.MIN_VALUE;
+        for (int n : dp) {
+            max = Math.max(max, n);
+        }
+        return max;
     }
 
+    /**
+     * 超出时间限制
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray1(int[] nums) {
+        int i = 0;
+        int j = 0;
+        int max = Integer.MIN_VALUE;
+        while (j < nums.length) {
+            i = 0;
+            while (i + j < nums.length) {
+                int sum = 0;
+                for (int k = i; k <= i + j; k++) {
+                    sum += nums[k];
+                }
+                max = Math.max(max, sum);
+                i++;
+            }
+            j++;
+        }
+        return max;
+    }
+
+
     public static void main(String[] args) {
-        MaximumSubarray maximumSubarray = new MaximumSubarray();
-        System.out.println(maximumSubarray.maxSubArray(new int[]{-1111, -10, -122, -12, -12, -24, -66}));
+
     }
 }
