65dae3c4ac98ced020fc3f2b694352322d210d76
Matthew-Han <837204579@qq.com>
2020-07-29 10:22:30 +0800

    面试题 17.01. 不用加号的加法

---
+++ eazy/面试题/AddWithoutPlus.java
@@ -0,0 +1,18 @@
+package 面试题;
+
+/**
+ * @ClassName AddWithoutPlus
+ * @Description 面试题 17.01. 不用加号的加法
+ * @Author MatthewHan
+ * @Date 2020/7/29 10:19
+ * @Version 1.0
+ **/
+public class AddWithoutPlus {
+
+    public int add(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return add(b ^ a, (a & b) << 1);
+    }
+}
