5fb184bf0a2b1f42a576d325f0585474fca94f02
Matthew-Han <837204579@qq.com>
2020-07-29 09:01:58 +0800

    #104 二叉树的最大深度

---
+++ eazy/二叉树/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,22 @@
+package 二叉树;
+
+/**
+ * @ClassName MaximumDepthOfBinaryTree
+ * @Description #104 二叉树的最大深度
+ * @Author MatthewHan
+ * @Date 2020/7/29 09:01
+ * @Version 1.0
+ **/
+public class MaximumDepthOfBinaryTree {
+
+    public static int maxDepth(TreeNode root) {
+        return bfs(root);
+    }
+
+    public static int bfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + Math.max(bfs(root.left), bfs(root.right));
+    }
+}
