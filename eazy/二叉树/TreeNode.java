6c2176609656c2190619dcdbbfd17ca9871d0fa9
Matthew-Han <837204579@qq.com>
2020-07-29 09:01:07 +0800

    二叉树公共类

---
+++ eazy/二叉树/TreeNode.java
@@ -0,0 +1,19 @@
+package 二叉树;
+
+/**
+ * @ClassName TreeNode
+ * @Description 二叉树公共类
+ * @Author MatthewHan
+ * @Date 2020/7/29 09:00
+ * @Version 1.0
+ **/
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
