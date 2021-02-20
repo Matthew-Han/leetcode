d3d5f4756f3d00d7fa9194ed529c8a26812214df
Matthew-Han <837204579@qq.com>
2021-02-20 17:49:00 +0800

    dream

--- dream/DegreeOfAnArray.java
+++ dream/DegreeOfAnArray.java
@@ -9,6 +9,78 @@ import java.util.*;
  **/
 public class DegreeOfAnArray {
 
+    /**
+     * 执行用时： 9 ms , 在所有 Java 提交中击败了 93.52% 的用户
+     * 内存消耗： 42 MB , 在所有 Java 提交中击败了 71.30% 的用户
+     *
+     * @param nums
+     * @return
+     */
+    public int findShortestSubArray(int[] nums) {
+        int[] bucket = new int[50000];
+        int max = 0;
+        int ans = Integer.MAX_VALUE;
+        // 存放众数的值（多个）
+        List<Integer> list = new ArrayList<>();
+        // 存放 nums[i] 的两端 index
+        Map<Integer, int[]> cache = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 初始化
+            if (!cache.containsKey(nums[i])) {
+                int[] v = new int[]{i, i};
+                cache.put(nums[i], v);
+            }
+
+            bucket[nums[i]]++;
+            if (max == bucket[nums[i]]) {
+                list.add(nums[i]);
+
+                // 更新右端点
+                int[] v = cache.get(nums[i]);
+                v[1] = i;
+                cache.put(nums[i], v);
+            } else if (max < bucket[nums[i]]) {
+                max = bucket[nums[i]];
+                list.clear();
+                list.add(nums[i]);
+
+                // 更新右端点
+                int[] v = cache.get(nums[i]);
+                v[1] = i;
+                cache.put(nums[i], v);
+            }
+
+
+        }
+        for (Integer n : list) {
+            int[] v = cache.get(n);
+            ans = Math.min(ans, v[1] - v[0] + 1);
+            // 这个就是最小数,提前退出,速度快不少
+            if (ans == max) {
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+    public int len(int[] nums, int n) {
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == n) {
+                left = i;
+                break;
+            }
+        }
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == n) {
+                right = i;
+                break;
+            }
+        }
+        return right - left + 1;
+    }
+
 
     /**
      * 执行用时 : 52 ms , 在所有 Java 提交中击败了 20.23% 的用户
@@ -17,7 +89,7 @@ public class DegreeOfAnArray {
      * @param nums
      * @return
      */
-    public static int findShortestSubArray(int[] nums) {
+    public static int findShortestSubArrayOld(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>(nums.length * 4 / 3 + 1);
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
@@ -55,6 +127,6 @@ public class DegreeOfAnArray {
     }
 
     public static void main(String[] args) {
-        System.out.println(findShortestSubArray(new int[]{1, 2, 1, 2, 3, 2}));
+        System.out.println(findShortestSubArrayOld(new int[]{1, 2, 1, 2, 3, 2}));
     }
 }
