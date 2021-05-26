3b5fdc2f13b07d36dd43975823a0300e08c6e28d
Matthew-Han <837204579@qq.com>
2021-05-26 11:19:40 +0800

    dream

--- dream/设计题/No146.java
+++ dream/设计题/No146.java
@@ -1,7 +1,9 @@
 package 设计题;
 
 import javafx.util.Pair;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 /**
@@ -69,6 +71,69 @@ public class No146 {
             }
         }
         System.out.println("data2 = " + data);
+    }
+
+    /**
+     * 解法二
+     * 比解法一稍微快一点, Hash 表少了循环判断 k - v 的存在
+     * 主要利用 <code>LinkedHashMap</code> 和 有序列表 <code>List</code> 来维护
+     * LinkedHashMap 维护缓存数据
+     * List 维护数据的有序性
+     *
+     * @see LinkedHashMap
+     */
+    public static class LruCache {
+
+        int limit;
+        Map<Integer, Integer> data;
+        Queue<Integer> keys;
+
+        public LruCache(int capacity) {
+            limit = capacity;
+            keys = new LinkedList<>();
+            data = new LinkedHashMap<>(capacity * 4 / 3 + 1);
+        }
+
+        public int get(int key) {
+            if (data.get(key) == null) {
+                return -1;
+            }
+            Integer v = data.get(key);
+            keys.remove(key);
+            keys.add(key);
+            data.remove(key);
+            data.put(key, v);
+            return v;
+        }
+
+        public void put(int key, int value) {
+            /* 判断一: data 为空 */
+            if (data.isEmpty()) {
+                data.put(key, value);
+                keys.add(key);
+                return;
+                /* 判断二: data 不为空, 但是长度小于 capacity */
+            } else if (data.size() < limit) {
+                if (data.containsKey(key)) {
+                    data.remove(key);
+                    keys.remove(key);
+                }
+                keys.add(key);
+                data.put(key, value);
+                return;
+            }
+            /* 判断三: data 目前长度为 capacity 需要分两种情况: 1. 当前缓存中有该 k - v; 2. 当前缓存中妹有该 k - v */
+            if (data.containsKey(key)) {
+                data.remove(key);
+                keys.remove(key);
+            } else {
+                data.remove(keys.peek());
+                keys.poll();
+            }
+            keys.add(key);
+            data.put(key, value);
+
+        }
     }
 
 
