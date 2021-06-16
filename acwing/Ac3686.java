6b6536e34c00a9e5b3d7746274e53ae949751678
Matthew-Han <837204579@qq.com>
2021-06-16 19:37:03 +0800

    dream

---
+++ acwing/Ac3686.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * @author <a href="https://github.com/Matthew-Han">Matthew Han</a >
+ * @date 2021/6/16 7:35 下午
+ * @since 1.0.0
+ **/
+public class Ac3686 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n-- > 0) {
+            int k = sc.nextInt();
+            int[] arr = new int[k];
+            for (int i = 0; i < k; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int l = 0;
+            int r = k - 1;
+            int ans = 0;
+            boolean flag = true;
+            boolean flag2 = true;
+            while (l < r) {
+                while (flag2 && arr[r] != 1) {
+                    r--;
+                }
+                flag2 = false;
+                if (flag && arr[l] == 1) {
+                    flag = false;
+                }
+                ans += !flag && arr[l] == 0 ? 1 : 0;
+                l++;
+            }
+            System.out.println(ans);
+
+        }
+    }
+}
