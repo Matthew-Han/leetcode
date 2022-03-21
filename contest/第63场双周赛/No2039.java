ade8b50d8cb26cc76d2e2b4f27b89464a1cdb98a
Matthew-Han <837204579@qq.com>
2022-03-21 10:16:50 +0800

    dream
    Signed-off-by: Matthew-Han <837204579@qq.com>

--- contest/第63场双周赛/No2039.java
+++ contest/第63场双周赛/No2039.java
@@ -9,6 +9,69 @@ import java.util.*;
  **/
 public class No2039 {
 
+    public static class Date20220321 {
+        Map<Integer, Integer> best = new HashMap<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+
+        public int networkBecomesIdle(int[][] edges, int[] patience) {
+            init(edges);
+            bfs();
+            int max = 0xc0c0c0c0;
+            for (Map.Entry<Integer, Integer> e : best.entrySet()) {
+                int curr = e.getKey();
+                if (curr == 0) {
+                    continue;
+                }
+                int size = e.getValue();
+                int reset = patience[e.getKey()];
+                int m = Math.max(0, size * 2 % reset == 0 ? size * 2 / reset - 1 : size * 2 / reset);
+                max = Math.max(max, reset * m + size * 2);
+            }
+            return max + 1;
+        }
+
+        public void init(int[][] edges) {
+            for (int[] edge : edges) {
+                // key1
+                map.put(edge[0], map.getOrDefault(edge[0], new ArrayList<>()));
+                List<Integer> v1 = map.get(edge[0]);
+                v1.add(edge[1]);
+
+                // key2
+                map.put(edge[1], map.getOrDefault(edge[1], new ArrayList<>()));
+                List<Integer> v2 = map.get(edge[1]);
+                v2.add(edge[0]);
+            }
+        }
+
+        public void bfs() {
+            HashSet<Integer> vis = new HashSet<>();
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(0);
+            vis.add(0);
+            int step = 0;
+            while (!q.isEmpty()) {
+                int limit = q.size();
+                for (int i = 0; i < limit; i++) {
+                    int curr = q.poll();
+                    best.put(curr, step);
+                    List<Integer> nxt = map.get(curr);
+                    if (nxt.isEmpty()) {
+                        continue;
+                    }
+                    for (int n : nxt) {
+                        if (vis.contains(n)) {
+                            continue;
+                        }
+                        q.offer(n);
+                        vis.add(n);
+                    }
+                }
+                step++;
+            }
+        }
+    }
+
     List<Integer> minPath;
 
     /**
