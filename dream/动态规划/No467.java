fad6c603164bd34c61dcf6f73890630ff8c3e39c
Matthew-Han <837204579@qq.com>
2022-05-26 14:53:06 +0800

    dream
    Signed-off-by: Matthew-Han <837204579@qq.com>

---
+++ dream/动态规划/No467.java
@@ -0,0 +1,39 @@
+package 动态规划;
+
+/**
+ * @author <a href="https://github.com/Matthew-Han">Matthew Han</a>
+ * @date 2022/5/26 14:50 26
+ * @since 1.0
+ **/
+public class No467 {
+
+    /**
+     * 环绕字符串中唯一的子字符串
+     * <p>
+     * 就是将每个开头字母能连接的最长串（最大值），26 个字母都加起来就行了。
+     *
+     * @param p
+     * @return
+     */
+    public int findSubstringInWraproundString(String p) {
+        int ans = 0;
+        int[] vis = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            int cnt = 1;
+            for (int j = i + 1; j < p.length(); j++) {
+                if (p.charAt(j - 1) == p.charAt(j) - 1 || p.charAt(j - 1) == p.charAt(j) + 25) {
+                    cnt++;
+                } else {
+                    break;
+                }
+            }
+            if (cnt > vis[p.charAt(i) - 'a']) {
+                vis[p.charAt(i) - 'a'] = cnt;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            ans += vis[i];
+        }
+        return ans;
+    }
+}
