80e674857c098c2be01f8feb622ed62b8dfd0478
Matthew Han <837204579@qq.com>
2023-03-15 16:02:50 +0800

    dream
    Signed-off-by: Matthew-Han <837204579@qq.com>

--- contest/第210场周赛/No1615.java
+++ contest/第210场周赛/No1615.java
@@ -1,7 +1,9 @@
 package 第210场周赛;
 
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * @author MatthewHan
@@ -10,6 +12,43 @@ import java.util.Map;
  * @date 2020/10/11 10:22
  **/
 public class No1615 {
+
+    public static class Date20230315 {
+        Map<Integer, Set<Integer>> map;
+
+        public int maximalNetworkRank(int n, int[][] roads) {
+            int max = 0;
+            map = new TreeMap<>();
+            for (int[] road : roads) {
+                Set<Integer> v1 = map.getOrDefault(road[0], new HashSet<>());
+                v1.add(road[1]);
+                Set<Integer> v2 = map.getOrDefault(road[1], new HashSet<>());
+                v2.add(road[0]);
+                max = Math.max(max, Math.max(v1.size(), v2.size()));
+                map.put(road[0], v1);
+                map.put(road[1], v2);
+            }
+            int ans = max;
+            for (Map.Entry<Integer, Set<Integer>> entry : map.entrySet()) {
+                int k1 = entry.getKey();
+                Set<Integer> v1 = entry.getValue();
+                if (v1.size() == max) {
+                    for (Map.Entry<Integer, Set<Integer>> entry2 : map.entrySet()) {
+                        int k2 = entry2.getKey();
+                        Set<Integer> v2 = entry2.getValue();
+                        if (k1 != k2) {
+                            ans = Math.max(ans, v1.size() + v2.size() - (check(k1, k2) ? 1 : 0));
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+
+        public boolean check(int o1, int o2) {
+            return map.getOrDefault(o1, new HashSet<>()).contains(o2);
+        }
+    }
 
 
     /**
